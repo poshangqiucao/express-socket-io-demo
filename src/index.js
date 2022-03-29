8343e2308f0e97f0791ca8882e5a138b93717cd1
poshang <2890122341@qq.com>
2022-03-29 10:52:06 +0800

    使用socket.io进行简单的前后端通信

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = 3000
+io.on('connection', (socket) => { 
+    console.log(socket.id);
+    socket.emit("hello", "world");
+    socket.on("hello1", (args) => {
+        console.log(args);
+    })
+});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/html/index.html')
+})
+
+server.listen(port, () => {
+    console.log(`server listen on port ${port}`);
+});
